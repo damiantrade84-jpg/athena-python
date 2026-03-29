41d4b8520ac73a8f61254cf0fc8954af43914d8b
damiantrade84-jpg <damiantrade84@gmail.com>
2026-03-29 18:29:39 +0200

    Fix SA Lotto number range: 52 -> 55

--- lottery_engine.py
+++ lottery_engine.py
@@ -22,9 +22,9 @@ LOTTERY_GAME_RULES = {
         "main_count": 6,
         "has_bonus": True,
         "main_min": 1,
-        "main_max": 52,
+        "main_max": 55,
         "bonus_min": 1,
-        "bonus_max": 52,
+        "bonus_max": 55,
     },
     "powerball": {
         "main_count": 5,
